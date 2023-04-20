f6d06206504062ac6302f1b7696ff15ac07be91c
chirag gaur <chiragwalker@gmail.com>
2023-04-20 10:19:21 +0530

    doubts

--- block-BJaadl/code/index.js
+++ block-BJaadl/code/index.js
@@ -10,37 +10,92 @@ let words = [
 ];
 
 // - Write a function findLongestWord that takes an array of words and returns the longest word from the array. (Use above array "words" to test it). If there are 2 with the same length, it should return the first occurrence.
-
+      // function findLongestWord(word){
+      //    word.filter((a,b) => {
+      //       return a.length - b.length;
+      //    });
+      // } 
+      // let out = findLongestWord(words);
 // - Convert the above array "words" into an array of length of word instead of word.
-
+    //  function conv(word){
+    //   return word.length;
+    //  }
+    //  let out = words.map(conv);
+    //  console.log(out);
 // - Create a new array that only contains word with atleast one vowel.
+    // doubt
+    //   let v = ['a','e','i','o','u'];
+    //   function vowel(val) {
+    //     return val.contains(v);
+    //   }
+    //  let vowl = words.filter(vowel);
 
 // - Find the index of the word "rhythm"
+    // console.log(words.indexOf("rhythm"));
 
 // - Create a new array that contians words not starting with vowel.
-
+      function notVowel(str) {
+        return str.contains('a');
+      }
+    let out = words.filter(notVowel);
+    console.log(out);
 // - Create a new array that contianse words not ending with vowel
-
+     // doubt
 let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers
+    //   function sumAll(acc,iv){
+    //       return acc + iv;
+    //   }
+    //  let out = numbers.reduce(sumAll);
+    //  console.log(out);
 
 // - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
-
+  //  function multiple(num){
+  //     return num * 3;
+  //  }
+  //     let out = numbers.map(multiple);
+  //     console.log(out);
 // - Create a new array that contains only even numbers
+// function even(num){
+//   return num % 2 === 0;
 
+// }
+// let out = numbers.filter(even);
+// console.log(out);
 // - Create  a new array that contains only odd numbers.
-
+  // function odd(num){
+  //       return num % 2 !== 0;
+     
+  //  }
+  //     let out = numbers.filter(odd);
+  //     console.log(out);
 // - Create a new array that should have true for even number and false for odd numbers.
-
+  // function even(num){
+  //        return num % 2 === 0;
+  //  }
+  //     let out = numbers.map(even);
+  //     console.log(out);
 // - Sort the above number in assending order.
-
+    //  function compare(a, b){
+    //    return a - b;
+    //  }
+    //  console.log(numbers.sort(compare));
 // - Does sort mutate the original array?
+     // yes it mutates the original array 
 
 // - Find the sum of the numbers in the array.
-
+    //   function sumAll(acc,iv){
+    //       return acc + iv;
+    //   }
+    //  let out = numbers.reduce(sumAll);
+    //  console.log(out);
 //- Write a function averageNumbers that receives an array of numbers and calculate the average of the numbers
-
+    //    function sumAll(acc,iv){
+    //       return (acc + iv / 2);
+    //   } 
+    //  let out = numbers.reduce(sumAll);
+    //  console.log(out);
 let strings = [
   'seat',
   'correspond',
@@ -55,3 +110,10 @@ let strings = [
 ];
 
 // - Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
+//   let total = 0;
+// function averageLength(str){
+//     total += str.length;
+//      return total;
+// }
+// let out = strings.forEach(averageLength);
+// console.log(total/10);
